f5afbc07297cf6cf885d7cff34c1b9e8095fa686
James Medley <81381425+jmed13@users.noreply.github.com>
2023-04-01 18:20:34 +0100

    Route.py finished

--- src/Route.py
+++ src/Route.py
@@ -3,9 +3,11 @@ import xmltodict
 with open("BUS_DATA/U1_U2-AVU1_AVU2--FBRI-BH-2023-02-19-V46_-_Bath-V42_-_Wells-BODS_V1_1.xml") as xml_file:
     route = xmltodict.parse(xml_file.read())
 
+
 def findBusStops(route):
     #returns list of bus stops on route, name and id
     return route["TransXChange"]["StopPoints"]["AnnotatedStopPointRef"]
+
 
 def findBusStopCoordinates(route):
     # returns list of coordinates for the bus stops
@@ -15,6 +17,7 @@ def findBusStopCoordinates(route):
         points = bus_stop["Track"]["Mapping"]["Location"]
         coordinates.append((points[0]["Longitude"], points[0]["Latitude"]))
     return coordinates
+
 
 def findRouteCoordinatesList(route):
     #returns list of coordinates for entire bus route
--- src/Timetable.py
+++
@@ -1,8 +0,0 @@
-import xmltodict
-
-with open("BUS_DATA/U1_U2-AVU1_AVU2--FBRI-BH-2023-02-19-V46_-_Bath-V42_-_Wells-BODS_V1_1.xml") as xml_file:
-    route = xmltodict.parse(xml_file.read())
-
-
-def findTimetables(route):
-    return
