a52ae812a9a3a287449f7c0660b1bee1d7f1af15
James Medley <81381425+jamesmedley@users.noreply.github.com>
2023-04-01 15:32:08 +0100

    Create U1_Route.py

---
+++ src/U1_Route.py
@@ -0,0 +1,16 @@
+import requests
+import zipfile, io, xmltodict
+import json
+url = "https://data.bus-data.dft.gov.uk/timetable/dataset/5813/download/"
+
+headers = {
+    'Authorization': 'Bearer 6ae6a620b33bf8bafe9b2f8f12326efcb8434206'
+}
+r = requests.get(url)
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall("BUS_DATA")
+
+with open("Routes/U1_U2-AVU1_AVU2--FBRI-BH-2023-04-09-W03_-_Wells-W03_-_Bath-BODS_V1_1.xml") as xml_file:
+    U1 = xmltodict.parse(xml_file.read())
+
+print(U1)
