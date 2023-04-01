7c3c8f85dab8932f8a98229ebc10e1bbaf3ae7f5
James Medley <81381425+jmed13@users.noreply.github.com>
2023-04-01 15:36:08 +0100

    U1 bus route py code

--- BusAPI.py
+++ BusAPI.py
@@ -12,7 +12,7 @@ headers = {
 
 r = requests.get(url)
 z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall("Routes")
+z.extractall("BUS_DATA")
 
 with open("Routes/U1_U2-AVU1_AVU2--FBRI-BH-2023-04-09-W03_-_Wells-W03_-_Bath-BODS_V1_1.xml") as xml_file:
     U1 = xmltodict.parse(xml_file.read())
