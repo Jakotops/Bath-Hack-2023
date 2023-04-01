a9c61f7607e373112c119a6474928482583d408d
James Medley <81381425+jmed13@users.noreply.github.com>
2023-04-01 17:37:00 +0100

    Update route.py

--- src/Timetable.py
+++ src/Timetable.py
@@ -1,6 +1,6 @@
 import xmltodict
 
-with open("BUS_DATA/U1_U2-AVU1_AVU2--FBRI-BH-2023-04-09-W03_-_Wells-W03_-_Bath-BODS_V1_1.xml") as xml_file:
+with open("BUS_DATA/U1_U2-AVU1_AVU2--FBRI-BH-2023-02-19-V46_-_Bath-V42_-_Wells-BODS_V1_1.xml") as xml_file:
     route = xmltodict.parse(xml_file.read())
 
 
