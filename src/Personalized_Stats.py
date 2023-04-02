bfdac510b506a2829c5c99289e15fcb5c6bd6a49
asv324 <77267950+asv324@users.noreply.github.com>
2023-04-02 08:41:14 +0100

    stats done

--- src/Personalized_Stats.py
+++ src/Personalized_Stats.py
@@ -1,8 +1,15 @@
 import json
 from Live_Bus_Data import findETA
+import requests
 
-def Total_Time_Spent_Travelling(init_pos, end_pos):
-    time = findETA(init_pos, end_pos)
+def Total_Time_Spent_Travelling(init_bus_stop, end_bus_stop):
+    response = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?destinations="
+                            + end_bus_stop[1] + ","
+                            + end_bus_stop[0] + "&origins="
+                            + init_bus_stop[1] + ","
+                            + init_bus_stop[0] + "&units=imperial&key=AIzaSyBkslyyGinzvl6kxBYojdlXOege8u1lDf4")
+    bus_data = response.json()
+    time = bus_data["rows"][0]["elements"][0]["duration"]["value"]
 
     with open("Stats.json", "r") as infile:
         jsonr = json.load(infile)
@@ -11,8 +18,14 @@ def Total_Time_Spent_Travelling(init_pos, end_pos):
     with open("Stats.json", "w") as outfile:
         json.dump(jsonr, outfile)
 
-def Total_Distance_Travelled(init_pos, end_pos):
-    distance = 0
+def Total_Distance_Travelled(init_bus_stop, end_bus_stop):
+    response = requests.get("https://maps.googleapis.com/maps/api/distancematrix/json?destinations="
+                            +end_bus_stop[1]+","
+                            +end_bus_stop[0]+"&origins="
+                            +init_bus_stop[1]+","
+                            +init_bus_stop[0]+"&units=imperial&key=AIzaSyBkslyyGinzvl6kxBYojdlXOege8u1lDf4")
+    bus_data = response.json()
+    distance = bus_data["rows"][0]["elements"][0]["distance"]["value"]
 
     with open("Stats.json", "r") as infile:
         jsonr = json.load(infile)
@@ -42,9 +55,23 @@ def Consecutive_Days():
         json.dump(jsonr, outfile)
 
 def Most_Frequent_Bus_On():
-    stop = ""
-    return stop
+    with open("Stats.json", "r") as infile:
+        jsonr = json.load(infile)
+
+    jsonr["most_freq_bus_on"] = max(set(jsonr["most_freq_busOn"]), key=jsonr["most_freq_busOn"].count)
+
+    with open("Stats.json", "w") as outfile:
+        json.dump(jsonr, outfile)
 
 def Most_Frequent_Bus_Off():
-    stop = ""
-    return stop
+    with open("Stats.json", "r") as infile:
+        jsonr = json.load(infile)
+
+    jsonr["most_freq_bus_off"] = max(set(jsonr["most_freq_busOff"]), key=jsonr["most_freq_busOff"].count)
+
+    with open("Stats.json", "w") as outfile:
+        json.dump(jsonr, outfile)
+
+
+Total_Distance_Travelled(('-2.324970', '51.379060'), ('-2.34360', '51.379960'))
+Total_Time_Spent_Travelling(('-2.324970', '51.379060'), ('-2.34360', '51.379960'))
